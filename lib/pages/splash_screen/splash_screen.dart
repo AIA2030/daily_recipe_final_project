9b07de028997f5da07242b4e9f24a651008e48bb
AIA2030 <rosa2371989@gmail.com>
2024-02-17 03:47:07 +0200

    daily_recipe_final_project

--- lib/pages/splash_screen/splash_screen.dart
+++ lib/pages/splash_screen/splash_screen.dart
@@ -1,29 +1,45 @@
+
 import 'dart:async';
 import 'package:daily_recipes_final/pages/main_pages/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreenPage extends StatefulWidget {
-  const SplashScreenPage({super.key});
+  const SplashScreenPage({Key? key});
 
   @override
   State<SplashScreenPage> createState() => _SplashScreenPageState();
 }
 
-class _SplashScreenPageState extends State<SplashScreenPage> {
+class _SplashScreenPageState extends State<SplashScreenPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
   StreamSubscription<User?>? _listener;
 
   @override
   void initState() {
-    initSplash();
     super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+    _controller.forward();
+    initSplash();
   }
 
   void initSplash() async {
-    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(Duration(seconds: 3));
     _listener = FirebaseAuth.instance.authStateChanges().listen((user) {
       if (user == null) {
-        Navigator.pushReplacement( context, MaterialPageRoute(builder: (_) => LogInPage()));
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => LogInPage()),
+        );
       } else {
         Navigator.pushReplacementNamed(context, '/home');
       }
@@ -33,6 +49,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   void dispose() {
     _listener?.cancel();
+    _controller.dispose();
     super.dispose();
   }
 
@@ -41,33 +58,52 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
+        child: FadeTransition(
+          opacity: _animation,
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:[
-
-                Container(
-
-                  child: Image.asset('assets/images/Logo (1).png', height: 222, width: 424, fit: BoxFit.fill),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Image.asset(
+                  'assets/images/Logo (1).png',
+                  height: 222,
+                  width: 424,
+                  fit: BoxFit.fill,
                 ),
-
-                SizedBox(height: 40,),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      child: Text("Cooking Done ", style: TextStyle(color: Colors.deepOrange, fontSize: 25.0, fontWeight: FontWeight.bold, fontFamily: 'Hellix'),),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      "Cooking Done ",
+                      style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Hellix',
+                      ),
                     ),
-
-                    Container(
-                      child: Text( "The Easy Way.", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.white, fontFamily: 'Hellix'),),
+                  ),
+                  Container(
+                    child: Text(
+                      "The Easy Way.",
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: 'Hellix',
+                      ),
                     ),
-
-                  ],
-                ),
-
-              ]
-          )
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
